185ec0d571d05c98d57d7556a4fe26d92233fb51
Yash <161423652+yashch09@users.noreply.github.com>
2024-12-19 23:28:46 +0530

    login.php

--- login.php
+++ login.php
@@ -125,7 +125,7 @@
 </div>
 </div>
  <script
-  src="http://code.jquery.com/jquery-3.3.1.min.js"
+  src="http://code.jquery.com/jquery-3.7.1.min.js"
   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
   crossorigin="anonymous"></script>
 <script type="text/javascript" src="Login.js"></script>
